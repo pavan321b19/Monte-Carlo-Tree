6c82f5a44dbd31a41308ca5ca4b4d402244cca04
RichmanRick <36971557+RichmanRick@users.noreply.github.com>
2022-11-26 20:53:33 -0700

    Initial implementation: select, expand, backprop..
    Wrote mcts agent functions for selecting next action, expanding decision tree, and stat backpropagation
    Some changes made to agent structure

--- MCTNode.py
+++ MCTNode.py
@@ -1,6 +1,6 @@
 # This class creates the underlying tree structure utilized by the MCTSAgent that
-# directly influences its decision making process. The mechanism or main steps of
-# MCTS are implemented here: Selection, Expansion, Simulation, Backpropagation.
+# directly influences its decision making process. Relevant class functions such as
+# creating children for nodes are added here.
 
 import itertools
 
@@ -9,38 +9,21 @@ class MCTNode():
     # provides an ID per node
     id = itertools.count()
     
-    def __init__(self, utility=None):
+    def __init__(self, action=None):
         self.children = []
         self.parent = None
         
-        self.utility = utility
-        self.visited = 1.
+        self.score = 0. # UCT evaluation based score
+        self.visits = 0
         self.win = 0 # how many games ended as wins going through this node
         self.loss = 0 # how many games ended as losses going through this node
         
-        self.action = None # one of pacman's actions
+        self.action = action # root holds none, otherwise holds pacman's action
         self.id = next(MCTNode.id)
     
     def addChild(self, child):
         self.children.append(child)
         child.parent = self
-    
-    # Backpropagates up tree and updates each MC tree node's statistic 
-    def update(node, result):
-        while True:
-            node.visited += 1.
-
-            if result:
-                node.win += 1
-            else:
-                node.loss += 1
-
-            # self.utility += utility
-            node.utility /= node.visited
-
-            # reached root node
-            if node.parent == None: break
-            node = node.parent
     
     # print out node's parent and children
     def print_relations(self):
--- MCTSAgent.py
+++ MCTSAgent.py
@@ -8,50 +8,94 @@ from game import Agent
 
 class MCTSAgent(Agent):
     """
-    A reflex agent chooses an action at each choice point by examining
-    its alternatives via a state evaluation function.
-
-    The code below is provided as a guide.  You are welcome to change
-    it in any way you see fit, so long as you don't touch our method
-    headers.
+    A MCTS agent's decision making is implemented with a decision tree, and it also involves some randomness. Every time an action is chosen, it simulates the rest of the game to help evaluate the actions chosen. It is expected for the agent to simulate the game numerous times in order to evaluate its decisions.
+    
+    The mechanism or main steps of MCTS are implemented here: Selection, Expansion, Simulation, Backpropagation.
     """
     
     def __init__(self):
-        self.root = MCTNode() # root node of tree used
+        self.root = MCTNode(None) # root node of tree used
         self.leaf = None # holds most recently expanded node
-    
-    # Selection: upper Confidence Bound selection process for agent's next action
+        self.threshold = 10 # minimum number of times each child is visited before using UCT to choose
+        
+        self.actions = [] # sequence of chosen actions for simulation
+
+    # Selection: selection process for agent's next action
     def selectAction(self, gameState):
-        # legalMoves = gameState.getLegalActions()
+        self.actions.clear() # clear current action sequence for next simulation
+        node = self.root
+        while True:
+            if node.action != None:
+                self.actions.append(node.action)
+            legalMoves = gameState.getLegalActions()
+            # reaching a leaf, expand randomly
+            if len(node.children) < len(legalMoves): # if len(node.children) == 0:
+                rand = random.randint(0, len(legalMoves)-1)
+                expandTree(node, legalMoves[rand])
+                return legalMoves[rand]
+            else:
+                # if all children's visits less than threshold, randomly select amongst them
+                if all(child.visits < self.threshold for child in node.children):
+                    rand = random.randint(0, len(node.children)-1)
+                    node = node.children[rand]
+                else:
+                    children = [(child, child.score) for child in node.children]
+                    node = max(children, key=lambda x:x[1])[0]
+            gameState = gameState.generatePacmanSuccessor(node.action)
+         
+        # score = successorGameState.getScore()
         # 
         # # Choose one of the best actions
         # scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
         # bestScore = max(scores)
         # bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
         # chosenIndex = random.choice(bestIndices) # Pick randomly among the best
-        # 
-        # "Add more of your code here if you want to"
-        # 
-        # return legalMoves[chosenIndex]
-        pass
     
     # Expansion: expand agent's tree after selection occurs
-    def expandTree(self, gameState):
-        # insert...
-        # self.leaf = inserted node
-        pass
+    def expandTree(self, node, action):
+        # first check if child exists or not
+        for child in node.children:
+            if child.action == action:
+                self.leaf = child
+                break
+        else:
+            expand = MCTNode(action)
+            node.addChild(expand)
+            self.leaf = expand
     
     # Simulation: after expansion let the agent play out randomly or by some method
-    def simulateActions(self, gameState):
-        # legalMoves = gameState.getLegalActions()
-        # randomMove = randomly select from legalMoves
-        # return randomMove
+    def simulateActions(self):
+        # start game with self.actions
+        # randomly choose actions until game ends or using an evaluation function of sorts
+        # return win/loss
         pass
         
-    # backpropagation to update tree statistics
-    def backpropagate(self, gameState):
-        # MCTNode.update(self.leaf, gameState.isWin())
-        pass
+    # Backpropagation: update tree nodes' statistics (UCT scores are updated here too)
+    def backpropagate(self, gameState): # backpropagate(self, simulation):
+        # self.leaf.update(simulation.isWin())
+        # self.leaf.update(gameState.isWin())
+        
+        node = self.leaf
+        # result = simulation.isWin()
+        while True:
+            node.visits += 1
+            
+            if result:
+                node.win += 1
+            else:
+                node.loss += 1
+            
+            # reached root node
+            if node.id == self.root.id:
+                break
+            
+            # UCT score = winratio + c* sqrt( ln(parent total) / (node total) )
+            win_ratio = node.win / (node.win + node.loss)
+            c = math.sqrt(2)
+            explore = math.sqrt(math.log(node.parent.visits + 1) / (node.visits))
+            self.score = win_ration + c* explore
+        
+            node = node.parent
     
 
     
